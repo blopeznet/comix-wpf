7bfcf6b311ad42f4463ead9b9f178310bb059fd3
blopez <blopez@fiveflamesmobile.com>
2018-06-23 01:12:28 +0200

    Implemented not found 404 css style search

---
+++ LocalFilesDatabase/DirectoryBrowser.WebServer/Server/PageNotFoundHandler.cs
@@ -0,0 +1,30 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.ViewEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryBrowser.WebServer.Server
+{
+    public class PageNotFoundHandler : DefaultViewRenderer, IStatusCodeHandler
+    {
+        public PageNotFoundHandler(IViewFactory factory) : base(factory)
+        {
+        }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound;
+        }
+
+        public void Handle(Nancy.HttpStatusCode statusCode, NancyContext context)
+        {
+            var response = RenderView(context, "PageNotFound");
+            response.StatusCode = HttpStatusCode.NotFound;
+            context.Response = response;
+        }
+    }
+}
